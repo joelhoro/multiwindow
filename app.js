1188cadc953dcd9691812bbb2da88e2c30cec9b7
Joel Horowitz <joelhoro@gmail.com>
2020-06-19 07:47:41 -0400

    Initial version of multiwindow

--- app.js
+++ app.js
@@ -1,77 +1,119 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, dialog} = require('electron')
 const path = require('path')
 const ipc = require('electron').ipcMain
 const fs = require('fs');
 
-var settings_file = 'settings.json'
+var settings_file = app.getAppPath() + '/settings/settings.json'
 
-var windows = []
+
+console.log("Hello")
 
 function createWindow (settings) {
+
+  const options = {
+    type: 'question',
+    buttons: ['Cancel', 'Yes, please', 'No, thanks'],
+    defaultId: 2,
+    title: settings_file,
+    message: settings_file,
+    detail: 'It does not really matter',
+    checkboxLabel: 'Remember my answer',
+    checkboxChecked: true,
+  };
+
+  dialog.showMessageBox(null, options, (response, checkboxChecked) => {
+    console.log(response);
+    console.log(checkboxChecked);
+  });
+
   if(!settings) {
-    settings = { size: { width: 600, height: 400 } };
+    settings = { size: { width: 900, height: 800 } };
   }
-  const mainWindow = new BrowserWindow({
-    width: settings.size[1],
-    height: settings.size[0],
+
+  let newWindow = new BrowserWindow({
+    width: settings.size[0],
+    height: settings.size[1],
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
     }
   })
   if(settings.position)
-    mainWindow.setPosition(settings.position[0],settings.position[1])
+    newWindow.setPosition(settings.position[0],settings.position[1])
+  
 
-  windows.push(mainWindow);
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + "/index.html")
-
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
-  mainWindow.setTitle("Window #" + windows.length);
-
-  function showCoordinates() {
-    console.log("======== Resize / Move =========");
-    console.log(windows);
-    windows.map(w => {
-      console.log("Window: ", w);
-      console.log(w.getTitle());
-      console.log("Size: ", w.getSize())
-      console.log("Position: ", w.getPosition())
-    })
-  }
+  newWindow.loadURL('file://' + __dirname + "/index.html")
+  //newWindow.webContents.openDevTools()
+  newWindow.setTitle("Window #" + BrowserWindow.getAllWindows().length);
+  newWindow.focus();
+  newWindow.webContents.on('dom-ready', () =>  {
+    if(settings.data)
+      newWindow.send('set', settings.data)
+  })
 
   // mainWindow.on('resize', showCoordinates);
   // mainWindow.on('move', showCoordinates);
-  mainWindow.on('closed', () => {
-    console.log("Closing window");
-    console.log("Windows: ", windows.length);
-    var index = windows.indexOf(mainWindow);
-    console.log("Deleting #", index);
-    windows.splice(index,1);
-    console.log("Windows: ", windows.length);
+  newWindow.on('close', () => {
+    newWindow = null;
   });
-  ipc.on('save-settings', () => {
-    showCoordinates();
-    var data = windows.map(w => ({
+
+}
+
+ipc.on('new-window', (evt,args) => {
+  createWindow();
+})
+
+ipc.on('debug', (evt,args) => {
+  debugger;
+  a = 123;
+  b = 123;
+})
+
+ipc.on('hello', (evt,args) => {
+  console.log(args);
+})
+
+function showCoordinates() {
+  console.log("======== Resize / Move =========");
+  //console.log(windows);
+  BrowserWindow.getAllWindows().map(w => {
+    //console.log("Window: ", w);    
+    console.log(w.getTitle());
+    console.log("Size: ", w.getSize())
+    console.log("Position: ", w.getPosition())
+    console.log("X: ", w.document)
+  })
+}
+
+var window_values = {}
+
+ipc.on('change', (evt,args) => {
+  window_values[evt.sender.id] = args;
+  console.log("Change: ", args)
+
+  //BrowserWindow.getAllWindows().map(w => w.send('set',args));
+});
+
+ipc.on('save-settings', (args) => {
+  
+  showCoordinates();
+  var data = BrowserWindow.getAllWindows().map(w => {
+    return {
+      data: window_values[w.id],
       title: w.getTitle(),
       size: w.getSize(),
       position: w.getPosition()
-    }));
-    try { 
-      fs.writeFileSync(settings_file, JSON.stringify(data), 'utf-8'); 
     }
-    catch(e) { 
-      console.error('Failed to save the file !'); 
-    }
-  })
-
-  ipc.on('new-window', (evt,args) => {
-    createWindow();
-  })
-}
+  });
+  try { 
+    console.log("Saving data: ", data);
+    fs.writeFileSync(settings_file, JSON.stringify(data), 'utf-8'); 
+  }
+  catch(e) { 
+    console.error('Failed to save the file !'); 
+  }
+})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -79,7 +121,7 @@ function createWindow (settings) {
 app.whenReady().then(() => {
   fs.readFile(settings_file, 'utf-8', function(err, data) {
     var settings = JSON.parse(data);
-    settings = [undefined];
+    //settings = [undefined];
     console.log(settings)
     settings.map(setting => {
       console.log("Setting: ", setting);
--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+
+const remote = require('electron');
+console.log("Remote: ", remote);
+var {ipcRenderer } = require('electron');
 const {Menu} = require('electron').remote;
-const ipc = require('electron').ipcRenderer
+
+function xyz() {
+    console.log("xyz");
+}
+
+ipcRenderer.on('request', function(){
+    ipcRenderer.send('hello', {a:123});
+    
+});
+
+// var authButton = document.getElementById('x');
+// authButton.addEventListener('click', function(){
+//     ipcRenderer.sendSync('new-window');
+// });
+
+// function x() {
+//     debugger;
+// }
+
+
 
 
 const menu = Menu.buildFromTemplate([{
@@ -8,16 +31,37 @@ const menu = Menu.buildFromTemplate([{
         {
             label: '&Save settings',
             click() {
-                ipc.send('save-settings');
+                ipcRenderer.send('save-settings');
             }
         },
         {
             label: '&New window',
             click() {
-                ipc.send('new-window');
+                console.log("Requesting new window");
+                ipcRenderer.send('new-window');
             }
         },
+        {
+            label: '&Quit',
+            click() {
+                remote.getCurrentWindow().close();
+            }
+        },
+
     ]
-}])
+},
+{    label: '&Debug',
+    click() {
+        debugger;
+    }
+
+},
+{    label: '&Debug remote',
+    click() {
+        ipcRenderer.send('debug');
+    }
+
+},
+])
 
 Menu.setApplicationMenu(menu);
